afcad7b485c05e6b3701c67b80eeaaab4304bc07
Felipe Suri <felipeheredia1234@gmail.com>
2020-03-28 19:00:16 -0300

    Bugs resolved

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -53,7 +53,20 @@ routes.get(
 	IncidentController.index
 );
 
-routes.post("/incidents", IncidentController.create);
+routes.post(
+	"/incidents",
+	celebrate({
+		[Segments.HEADERS]: Joi.object({
+			authorization: Joi.string().required()
+		}).unknown(),
+		[Segments.BODY]: Joi.object().keys({
+			title: Joi.string().required(),
+			description: Joi.string().required(),
+			value: Joi.number().required()
+		})
+	}),
+	IncidentController.create
+);
 
 routes.delete(
 	"/incidents/:id",
